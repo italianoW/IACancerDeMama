43ebad8d1d514594b4e5988ad97c9814aa5e612f
Wendel Italiano <145154652+italianoW@users.noreply.github.com>
2025-09-19 12:17:30 -0300

    Update test_train.py

--- tests/test_train.py
+++ tests/test_train.py
@@ -4,11 +4,8 @@ import model
 
 def test_treino():
     
-    local_model = model.provide_model()
+    local_model = model.model()
     
     train.train_model(local_model)
-    
-    # Criem um atributo _is_fitted no modelo quando treinarem:
-    # model._is_fitted = True 
-    #Verifica se o modelo foi treinado 
+     
     assert hasattr(local_model, "_is_fitted") and local_model._is_fitted, "O modelo não foi treinado"
