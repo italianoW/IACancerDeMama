8ba9cd615dcb99dc4addd5f2b371e44baeb6bd11
Wendel Italiano <145154652+italianoW@users.noreply.github.com>
2025-09-19 12:16:34 -0300

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,24 +1,11 @@
 import model
-import tensorflow as tf
-from tensorflow import keras
 
-def test_model_structure():
-    
-    local_model = model.provide_model()
-  
-    #Verifica se o retorno da função obter_modelo retorna mesmo um modelo Keras.   
-    assert (isinstance(local_model,keras.Model)), "provide_model não retorna um modelo Keras."
-    
-    #Verifica se a última camada é Dense.
-    assert isinstance(local_model.layers[-1], keras.layers.Dense), "Última camada não é Dense"
-    
-    #Verifica se a última camada do modelo retorna um one-hot de tamanho 2.
-    assert (local_model.layers[-1].units == 2), "A última camada do modelo não retorna um one-hot de 2 valores."
-    
-    #Verifica se a última camada do modelo aplica Softmax para categorização correta.
-    assert (local_model.layers[-1].activation.__name__ == "softmax"), "A última camada não usa softmax"
+def test_model():
 
-def test_save_model():
-    
-    #TODO
-    assert 1
+  local_model_structure = model.model()
+
+  assert local_model is not None, "O modelo não foi carregado corretamente"
+  assert (isinstance(local_model,tf.keras.Model)), "provide_model não retorna um modelo Keras."
+  assert isinstance(local_model.layers[-1], tf.keras.layers.Dense), "Última camada não é Dense"
+  assert (local_model.layers[-1].units == 2), "A última camada do modelo não retorna um one-hot de 2 valores."
+  assert (local_model.layers[-1].activation.__name__ == "softmax"), "A última camada não usa softmax"
